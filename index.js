275a729aec1828638bd6252f377c1599ef81d1e9
Camila <salinicamila@gmail.com>
2025-05-27 17:25:22 -0300

    se agrega index con funcion gestor

---
+++ index.js
@@ -0,0 +1,47 @@
+const url_base = 'https://fakestoreapi.com';
+const argv = process.argv;
+const metodo = argv[2];
+const ruta = argv[3];
+const args = argv.slice(4);
+
+function gestor_productos() {
+    try {
+        if (metodo === 'GET' && ruta === 'products') {
+            fetch(`${url_base}/products`)
+                .then(response => response.json())
+                .then(data => console.log(data));
+
+        } else if (metodo === 'GET' && ruta.startsWith('products/')) {
+            const id = ruta.split('/')[1];
+            fetch(`${url_base}/products/${id}`)
+                .then(response => response.json())
+                .then(data => console.log(data));
+
+        } else if (metodo === 'POST' && ruta === 'products') {
+            const [title, price, category] = args;
+            fetch(`${url_base}/products`, {
+                method: 'POST',
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify({title, price, category})
+            })
+                .then(response => response.json())
+                .then(data => console.log(data));
+
+        } else if (metodo === 'DELETE') {
+            const id = ruta.split('/')[1];
+            fetch(`${url_base}/products/${id}`, {
+                method: 'DELETE'
+            })
+                .then(response => response.json())
+                .then(data => console.log(data))
+
+        } else {
+            console.log('Comando no reconocido.');
+        }
+
+    } catch (err) {
+        console.error('Error:', err.message);
+    }
+}
+
+gestor_productos();
